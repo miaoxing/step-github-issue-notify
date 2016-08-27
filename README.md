87beb067852aee51cff3447493d551fee067e13a
twinh <twinhuang@qq.com>
2016-08-27 10:56:00 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Github issue notify
+
+Create github issue when build failed
+
+# Options
+
+- `token` - The github personal token to create issues https://github.com/settings/tokens
+- `repo` - The github repository to create issues
+
+# Example
+
+```yaml
+build:
+    after-steps:
+        - miaoxing/github-issue-notify
+            token: $GITHUB_ISSUE_TOKEN
+            repo: username/repo
+```
+
+# License
+
+The MIT License (MIT)
+
+# Changelog
+
+## 0.0.1
+
+- Initial release
---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+echo 'Build' $WERCKER_RESULT;
+
+if [ $WERCKER_RESULT = 'passed' ] ; then
+  exit
+fi
+
+if [ ! -n "$WERCKER_GITHUB_ISSUE_REPO" ]; then
+  $WERCKER_GITHUB_ISSUE_REPO=$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY
+fi
+
+title='Build failed: '$(git log -1 --pretty=%B)
+
+assignee=$(git log -1 --pretty=%cn)
+
+body="Result: $WERCKER_RESULT
+
+Failed step: $WERCKER_FAILED_STEP_DISPLAY_NAME - $WERCKER_FAILED_STEP_MESSAGE
+
+View changeset: $WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY@$WERCKER_GIT_COMMIT
+
+View build log: $WERCKER_RUN_URL"
+body=${body//\'/\\\'}
+body=${body//\"/\\\"}
+body=${body//
+/\\\n}
+
+data="{\"title\": \"$title\",\"body\":\"$body\",\"assignees\":[\"$assignee\"],\"labels\": [\"task\"]}"
+echo $data
+
+curl -i -H "Authorization: token $WERCKER_GITUHB_ISSUE_TOKEN" -d "$data" \
+https://api.github.com/repos/$WERCKER_GITHUB_ISSUE_REPO/issues
