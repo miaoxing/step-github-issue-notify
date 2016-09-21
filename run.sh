325567f8860b64ba8dfd32155f9ba97effe98e2d
twinh <twinhuang@qq.com>
2016-09-21 15:28:24 +0800

    update body message

--- run.sh
+++ run.sh
@@ -10,19 +10,21 @@ fi
 if [ ! -n "$WERCKER_GITHUB_ISSUE_NOTIFY_REPO" ]; then
   WERCKER_GITHUB_ISSUE_NOTIFY_REPO="$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY"
 fi
-info "Notify repo: $WERCKER_GITHUB_ISSUE_NOTIFY_REPO"
+info "notify repo: $WERCKER_GITHUB_ISSUE_NOTIFY_REPO"
 
-title="Build failed: $(git log -1 --pretty=%B)"
-
+message=$(git log -1 --pretty=%B)
+title="Build failed: $message"
 assignee=$(git log -1 --pretty=%cn)
+body="Status: $WERCKER_RESULT
 
-body="Result: $WERCKER_RESULT
+Author: $assignee
+Message: $title
 
 Failed step: $WERCKER_FAILED_STEP_DISPLAY_NAME - $WERCKER_FAILED_STEP_MESSAGE
 
-View changeset: $WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY@$WERCKER_GIT_COMMIT
+View the changeset: $WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY@$WERCKER_GIT_COMMIT
 
-View build log: $WERCKER_RUN_URL"
+View the full build log and details: $WERCKER_RUN_URL"
 body=${body//\'/\\\'}
 body=${body//\"/\\\"}
 body=${body//
